c0862afe5c89c0ba62c0f58c1f17a18055432f30
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-10-14 09:53:14 +0200

    add Soldes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
-import './styles/App.css';
-import Banner from './Components/Banner';
-import Cart from './Components/Cart';
-import ShoppingList from './Components/ShoppingList';
+import Banner from './Components/Banner'
+// import Cart from './Cart'
+import ShoppingList from './Components/ShoppingList'
 
 function App() {
-  return (
-    <div>
-      <Banner />
-      <Cart />
-      <ShoppingList />
-    </div>
-  );
+	return (
+		<div>
+			<Banner />
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/Components/ShoppingList.jsx
+++ src/Components/ShoppingList.jsx
@@ -8,6 +8,7 @@ function ShoppingList() {
 		[]
 	)
 
+
 	return (
 		<div>
 			<ul>
@@ -19,6 +20,7 @@ function ShoppingList() {
 				{plantList.map((plant) => (
 					<li key={plant.id} className='lmj-plant-item'>
 						{plant.name}
+						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
 					</li>
 				))}
 			</ul>
