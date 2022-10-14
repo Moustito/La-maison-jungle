54a3ebdd9c65ca4df1eb00b3afeced93a286f381
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-10-14 11:56:03 +0200

    add alert for water and light

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Banner from './Components/Banner'
-// import Cart from './Cart'
+import logo from './assets/logo.png'
+// import Cart from './Components/Cart'
+import Footer from './Components/Footer'
 import ShoppingList from './Components/ShoppingList'
 
 function App() {
 	return (
 		<div>
-			<Banner />
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+
 			{/* <Cart /> */}
 			<ShoppingList />
+			<Footer />
 		</div>
 	)
 }
