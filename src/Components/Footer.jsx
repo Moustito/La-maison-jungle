06890c150e7c3ef74f7e1e10c4d5e6feb8c206a0
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-10-14 13:22:15 +0200

    add mail verification

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,26 @@ import '../styles/Footer.css'
 function Footer() {
 	const [inputValue, setInputValue] = useState('')
 
+	function checkValue(value) {
+		console.log(value);
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide.");
+		}
+	}
+
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<input
+				type="mail"
+				placeholder='exemple@mail.com'
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+				onBlur={(event) => checkValue(event.target.value)}
+            />
 		</footer>
 	)
 }
