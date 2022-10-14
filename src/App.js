cd68915452457a1d04721d7e9f2aa824f9254e5d
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-10-14 09:00:51 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './styles/App.css';
 import Banner from './Components/Banner';
 import Cart from './Components/Cart';
+import ShoppingList from './Components/ShoppingList';
 
 function App() {
   return (
     <div>
       <Banner />
       <Cart />
+      <ShoppingList />
     </div>
   );
 }
