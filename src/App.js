18cd5e23f78d32926c8537159adfe43ea45cc871
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-10-17 09:47:02 +0200

    create a select and reinitilize button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import Banner from './Components/Banner'
 import logo from './assets/logo.png'
-// import Cart from './Components/Cart'
+import Cart from './Components/Cart'
 import Footer from './Components/Footer'
 import ShoppingList from './Components/ShoppingList'
+import './styles/Layout.css'
+import { useState } from 'react'
 
 function App() {
+	const [cart, updateCart] = useState([])
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-
-			{/* <Cart /> */}
-			<ShoppingList />
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
 			<Footer />
 		</div>
 	)
